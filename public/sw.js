a4c493d70c7692d9dc4c1dee3f5aa06cacecb5c0
GrabbitGames <154015851+GrabbitGames@users.noreply.github.com>
2024-02-28 07:05:18 -0500

    Delete public directory

--- public/sw.js
+++
@@ -1,8 +0,0 @@
-importScripts("/public/uv/uv.bundle.js");
-importScripts("/public/uv/uv.config.js");
-importScripts("/public/uv/uv.sw.js");
-
-const sw = new UVServiceWorker();
-let userKey = new URL(location).searchParams.get('userkey');
-
-self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
